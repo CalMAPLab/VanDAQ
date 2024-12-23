4ed66243c9c05b32d8d9f966c0acb9b85108c105
robinweber <rjweber@berkeley.edu>
2024-12-23 17:24:39 +0000

    Success in running dashboard  page build in background, cached

--- web/DashPlay_pages.py
+++ web/DashPlay_pages.py
@@ -3,15 +3,9 @@ from dash import Dash, dcc, html, Input, Output, State, dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from numpy import isnan
-import random
 import plotly.graph_objs as go
-import pandas as pd
-import datetime
 import yaml
-#from vandaq_measurements_query import get_measurements
-from vandaq_2step_measurements_query import get_2step_query_with_alarms
-from vandaq_2step_measurements_query import transform_instrument_dataframe
-from sqlalchemy import create_engine, and_
+from sqlalchemy import create_engine
 
 # import other dashboard pages
 from Dash_Alarm_Table import *
@@ -20,14 +14,16 @@ from Dash_Dashboard import *
 # Initialize the Dash app
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
-configfile_name = '/home/vandaq/vandaq/web/DashPlay.yaml'
-try:
-    configfile = open(configfile_name,'r')
-    config = yaml.load(configfile, Loader=yaml.FullLoader)
-    configfile.close()
-except:
-    print("Cannot load config file "+configfile_name)
-    exit()
+def get_config(): 
+    configfile_name = '/home/vandaq/vandaq/web/DashPlay.yaml'
+    try:
+        configfile = open(configfile_name,'r')
+        config = yaml.load(configfile, Loader=yaml.FullLoader)
+        configfile.close()
+    except:
+        print("Cannot load config file "+configfile_name)
+        exit()
+    return config
 
 # Database connection
 engine = create_engine('postgresql://vandaq:p3st3r@localhost:5432/vandaq-dev', echo=False)
@@ -41,6 +37,9 @@ app.layout = html.Div([
     html.Div(id='tab-content')
 ])
 
+update_dashboard(app, engine, get_config())
+update_alarm_table(app, engine, get_config())
+
 # Callback to update tab content
 @app.callback(
     Output('tab-content', 'children'),
@@ -49,15 +48,11 @@ app.layout = html.Div([
 )
 def render_tab(tab_name):
     if tab_name == 'dashboard':
-        return layout_dashboard()
+        return layout_dashboard(get_config())
     elif tab_name == 'alarm-table':
-        ret = layout_alarm_table()
+        ret = layout_alarm_table(get_config())
         return ret
 
-
-
-update_dashboard(app, engine, config)
-update_alarm_table(app, engine, config)
 
 # Run the app
 server = app.server
--- web/Dash_Dashboard.py
+++ web/Dash_Dashboard.py
@@ -1,12 +1,14 @@
 from dash import dcc, html, Input, Output, State, dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 from numpy import isnan
-import random
 import plotly.graph_objs as go
 import pandas as pd
 import datetime
-import yaml
+import time
+import copy
+from threading import Thread, Lock
 #from vandaq_measurements_query import get_measurements
 from vandaq_2step_measurements_query import get_2step_query_with_alarms
 from vandaq_2step_measurements_query import transform_instrument_dataframe
@@ -14,6 +16,9 @@ from vandaq_2step_measurements_query import transform_instrument_dataframe
 
 
 sample_time = datetime.datetime.now()
+
+config = None
+engine = None
 
 def get_last_valid_value(df, column):
     last_valid_index = df[column].last_valid_index()
@@ -62,7 +67,7 @@ def create_trend_plot_old(instrument_data_list):
         showlegend=False  # Hides the legend
     )
 
-def create_trend_plot(instrument_data_list):
+def create_trend_plot(instrument_data_list, config):
     graphs = []
     shapes = []  # List to hold the background shapes for alarm levels
     i = 0
@@ -84,23 +89,24 @@ def create_trend_plot(instrument_data_list):
         graphs.append(graph)
 
         # Add background shapes for max_alarm_level > 0
-        if 'max_alarm_level' in data.columns:
-            alarm_intervals = data[data['max_alarm_level'] > 0]
-            for idx, row in alarm_intervals.iterrows():
-                color = 'rgba(255,0,0,0.6)' if row['max_alarm_level'] == 2 else 'rgba(255,255,0,0.6)'
-                shapes.append({
-                    'type': 'rect',
-                    'xref': 'x',  # Use x-axis as reference
-                    'yref': 'paper',  # Use the paper height as reference for full y-axis coverage
-                    'x0': idx,  # Start time of the interval
-                    'x1': idx + pd.Timedelta(seconds=1),  # End time of the interval
-                    'y0': 0,
-                    'y1': 1,
-                    'fillcolor': color,
-                    'opacity': 0.6,
-                    'layer': 'below',  # Ensure it appears below the line plot
-                    'line_width': 0
-                })
+        if 'alarm_shapes' in config and config['alarm_shapes']:
+            if 'max_alarm_level' in data.columns:
+                alarm_intervals = data[data['max_alarm_level'] > 0]
+                for idx, row in alarm_intervals.iterrows():
+                    color = 'rgba(255,0,0,0.6)' if row['max_alarm_level'] == 2 else 'rgba(255,255,0,0.6)'
+                    shapes.append({
+                        'type': 'rect',
+                        'xref': 'x',  # Use x-axis as reference
+                        'yref': 'paper',  # Use the paper height as reference for full y-axis coverage
+                        'x0': idx,  # Start time of the interval
+                        'x1': idx + pd.Timedelta(seconds=1),  # End time of the interval
+                        'y0': 0,
+                        'y1': 1,
+                        'fillcolor': color,
+                        'opacity': 0.6,
+                        'layer': 'below',  # Ensure it appears below the line plot
+                        'line_width': 0
+                    })
 
         i += 1
 
@@ -178,41 +184,47 @@ def get_list_of_items(engine, config):
     for instrument in measurements:
         seconds_ago = 0
         instrument_text = list(instrument.keys())[0]
-        graph_params = config['display_params'][instrument_text]['graph']
-        graph_data = [{'parameter':param['parameter'], 'measurements':param['measurements']} for param in instrument[instrument_text] if param['parameter'] in graph_params]
-        graph = create_trend_plot(graph_data)
-        alarm_level = max([max(list(data['measurements']['max_alarm_level'][-5:])) for data in graph_data])
-        alarm_box_style = None
-        if alarm_level == 2:
-            alarm_box_style = 'flashing-box-alarm'
-        elif alarm_level == 1:
-            alarm_box_style = 'flashing-box-warning'
-        instrument_name = html.H2(instrument_text.replace('_',' '), className=alarm_box_style)
-        
-        reading_cells = [instrument_name]
-        for parameter in instrument[instrument_text]:
-            parameter_text = parameter['parameter']
-            do_display = False
-            if parameter_text in config['display_params'][instrument_text]['display']:
-                do_display = True
-                reading_string = parameter_text + ': ' + '{:.4f}'.format(get_last_valid_value(parameter['measurements'],'value')) + ' '+ parameter['unit']
-                reading_line = None
-                if 'engineering' in parameter['acquisition_type']:
-#                   reading_line = html.Div(reading_string,className='engineering_reading')  
-                    reading_line = None  
-                else:
-                    if do_display:
-                        reading_line = html.Div(reading_string,className='ambient_reading')
+        if instrument_text in config['display_params']:
+            graph_params = config['display_params'][instrument_text]['graph']
+            graph_data = [{'parameter':param['parameter'], 'measurements':param['measurements']} for param in instrument[instrument_text] if param['parameter'] in graph_params]
+            graph = None
+            graph = create_trend_plot(graph_data, config)
+            alarm_level = max([max(list(data['measurements']['max_alarm_level'][-5:])) for data in graph_data])
+            alarm_box_style = None
+            if alarm_level == 2:
+                alarm_box_style = 'flashing-box-alarm'
+            elif alarm_level == 1:
+                alarm_box_style = 'flashing-box-warning'
+            instrument_name = html.H2(instrument_text.replace('_',' '), className=alarm_box_style)
+            
+            reading_cells = [instrument_name]
+            for parameter in instrument[instrument_text]:
+                parameter_text = parameter['parameter']
+                do_display = False
+                if parameter_text in config['display_params'][instrument_text]['display']:
+                    do_display = True
+                    # Vulnerability here v
+                    try:
+                        reading_string = parameter_text + ': ' + '{:.4f}'.format(get_last_valid_value(parameter['measurements'],'value')) + ' '+ parameter['unit']
+                    except TypeError as e:
+                        reading_string = 'Bad Value: '+ str(get_last_valid_value(parameter['measurements'],'value'))
+                    reading_line = None
+                    if 'engineering' in parameter['acquisition_type']:
+    #                   reading_line = html.Div(reading_string,className='engineering_reading')  
+                        reading_line = None  
                     else:
-                        reading_line = None
-                if reading_line:
-                    reading_cells.append(reading_line)
-                sample_time = get_last_valid_value(parameter['measurements'],'sample_time')
-                #if 'seconds_ago' in reading.keys():
-                #    seconds_ago = reading['seconds_ago']
-            #if seconds_ago > 1:
-            #    reading_cells.insert(1,'as of '+str(seconds_ago)+' seconds ago')
-        items.append(create_grid_cell(graph,reading_cells))
+                        if do_display:
+                            reading_line = html.Div(reading_string,className='ambient_reading')
+                        else:
+                            reading_line = None
+                    if reading_line:
+                        reading_cells.append(reading_line)
+                    sample_time = get_last_valid_value(parameter['measurements'],'sample_time')
+                    #if 'seconds_ago' in reading.keys():
+                    #    seconds_ago = reading['seconds_ago']
+                #if seconds_ago > 1:
+                #    reading_cells.insert(1,'as of '+str(seconds_ago)+' seconds ago')
+            items.append(create_grid_cell(graph,reading_cells))
 
     return items, sample_time
 
@@ -223,32 +235,71 @@ local_styles ={
     'color':'white'
 }
 
-refresh_secs = 2
+refresh_secs = 1
 
 # Layout for the dashboard page
-def layout_dashboard():
+def layout_dashboard(config):
     return html.Div([
-        dcc.Interval(id='interval-component', interval=refresh_secs * 1000, n_intervals=0),
+        dcc.Interval(id='interval', interval=refresh_secs * 1000, n_intervals=0),
+        dcc.Store(id="cache-timestamp", data=None),  # To store the last seen timestamp
         html.H1('VanDAQ Operator Dashboard', style={'text-align': 'left'}),
         html.Div(id='sample_timestamp'),
-        html.Div(id='grid-container')
+        html.Div(id='grid-container', children=['Awaiting data...'])
     ])
 
-def update_dashboard(app, engine, config): 
+
+latest_page = None
+latest_page_time = None
+
+def update_dashboard(app, engine, config):
+
+    lock = Lock()
+
+
+    Thread(target=regenerate_page, args=(engine, config, lock), daemon=True).start()
+
     @app.callback(
-        Output('grid-container', 'children'),
-        Output('sample_timestamp', 'children'),
-        Input('interval-component', 'n_intervals'),
-        suppress_callback_exceptions=True
+        [
+            Output('grid-container', 'children'),
+            Output('sample_timestamp', 'children'),
+            Output("cache-timestamp", "data"),
+        ],
+        [
+            Input("interval", "n_intervals"),
+            State("cache-timestamp", "data")
+        ]
     )
-    def update_grid(n):
+    def update_page(n_intervals, last_seen_timestamp):
+        global latest_page
+        global latest_page_time
+        stime = datetime.datetime.now()
+        print('checking for update')
+        if isinstance(last_seen_timestamp, str):
+            last_seen_timestamp = datetime.datetime.strptime(last_seen_timestamp,'%Y-%m-%dT%H:%M:%S.%f')
+        with lock:
+            cached_timestamp = latest_page_time
+        if cached_timestamp and ((last_seen_timestamp is None) or (cached_timestamp > last_seen_timestamp)):
+            with lock:
+                cached_timestamp = latest_page_time
+                cached_page = copy.copy(latest_page)
+            sample_timestamp = f'Last sample time (UTC): {cached_timestamp.strftime("%m/%d/%Y, %H:%M:%S")}'
+            etime = datetime.datetime.now()
+            print(f'got page in {(etime-stime).total_seconds()} secs')
+            return cached_page, sample_timestamp, cached_timestamp
+        raise PreventUpdate
+
+# Periodically regenerate the page content in the background
+def regenerate_page(engine, config, lock):
+    while True:
+        #stime = datetime.datetime.now()
         items, sample_time = get_list_of_items(engine, config)
-        num_rows = min(5, len(items))  # You can adjust the number of rows if needed
-        
-        # Generate a list of Div elements for the grid
-        #grid_items = [html.Div(item, className="grid-item") for item in items]
-        grid_items = items
-        
-        sample_timestamp = ' Last sample time (UTC): '+ sample_time.strftime("%m/%d/%Y, %H:%M:%S")
-        # Return the grid layout with inline CSS
-        return grid_items, sample_timestamp
+        global latest_page
+        global latest_page_time
+        with lock:
+            latest_page = items
+            latest_page_time = datetime.datetime.now()
+        #etime = datetime.datetime.now()
+        #print(f'rebuilt page in {(etime-stime).total_seconds()} secs')
+        time.sleep(0.1)  # Keep refreshing every second
+
+
