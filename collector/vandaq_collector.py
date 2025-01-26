2576870ace9d65bc45a2c98f06d291c14757673f
robinweber <rjweber@berkeley.edu>
2025-01-26 18:24:38 +0000

    Implemented batch insert in collector for performance

--- collector/vandaq_collector.py
+++ collector/vandaq_collector.py
@@ -13,10 +13,12 @@ import shutil
 from glob import glob
 from datetime import datetime, timedelta
 from logging.handlers import TimedRotatingFileHandler
-from sqlalchemy import create_engine, and_
+from sqlalchemy import create_engine, and_, text, insert
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker, relationship
 from vandaq_schema import *
+from collections import defaultdict
+
 
 first_insert = True
 platform_dict = None
@@ -280,6 +282,248 @@ def insert_measurment_into_database(session, message):
         logger.error(f"Failed to insert data due to integrity constraint violation - {e}.")
         return False
 
+class Inserter:
+    def __init__(self, session, config):
+        self.session = session
+        self.config = config
+        self.dimension_cache = {
+            'platform': {},
+            'instrument': {},
+            'parameter': {},
+            'unit': {},
+            'acquisition_type': {},
+            'alarm_type': {},
+            'alarm_level': {},
+            'time': {},
+            'geolocation':{}
+        }
+        self.insert_batch_seconds = self.config.get('insert_batch_seconds',1)
+        self.cache_time_seconds = self.config.get('cache_time_seconds', 3600)
+        self.load_dimension_cache()
+        
+    def load_dimension_cache(self):
+        self.dimension_cache["platform"] = {
+            record.platform: record.id for record in self.session.query(DimPlatform).all()
+        }
+        self.dimension_cache["instrument"] = {
+            record.instrument: record.id for record in self.session.query(DimInstrument).all()
+        }
+        self.dimension_cache["parameter"] = {
+            record.parameter: record.id for record in self.session.query(DimParameter).all()
+        }
+        self.dimension_cache["unit"] = {
+            record.unit: record.id for record in self.session.query(DimUnit).all()
+        }
+        self.dimension_cache["acquisition_type"] = {
+            record.acquisition_type: record.id for record in self.session.query(DimAcquisitionType).all()
+        }
+        self.dimension_cache["alarm_type"] = {
+            record.alarm_type: record.id for record in self.session.query(DimAlarmType).all()
+        }
+        self.dimension_cache["alarm_level"] = {
+            record.alarm_level: record.id for record in self.session.query(DimAlarmLevel).all()
+        }
+
+    def get_or_create_record(self, session, model, lookup_field, value, cache_dict):
+        record_id = cache_dict.get(value)
+        if not record_id:
+            record = session.query(model).filter(getattr(model, lookup_field) == value).first()
+            if not record:
+                record = model(**{lookup_field: value})
+                session.add(record)
+                session.flush()  # Ensure the ID is generated
+            record_id = record.id
+            cache_dict[value] = record_id
+        return record_id    
+
+    def get_or_create_dimension(self, model, field, value, cache_key):
+        return self.get_or_create_record(self.session, model, field, value, self.dimension_cache[cache_key])
+
+    def get_or_create_time_dimension(self, time):
+        if time is None:
+            return None
+        time = time.replace(microsecond=0)
+        time_id = self.dimension_cache['time'].get(time);
+        if time_id:
+            return time_id
+        # if this time is not in the time dimension cache, 
+        # attempt to retrieve times cache_time_seconds before 
+        # and after the given time
+        start_time = time - timedelta(seconds=self.cache_time_seconds) 
+        end_time = time + timedelta(seconds=self.cache_time_seconds)
+
+        times_in_db = self.session.query(DimTime).where(
+            and_((DimTime.time >= start_time),
+            (DimTime.time <= end_time))      
+        ).all()
+        times_in_table = [rec.time.replace(microsecond=0) for rec in times_in_db]
+        # generate a list of seconds between the start and end
+        times = [start_time.replace(microsecond=0) + timedelta(seconds=i)
+            for i in range(int((end_time - start_time).total_seconds()) + 1)]
+        # make a list of times not in the time dimension table
+        missing_times = [{'time':newTime} for newTime in times if newTime not in times_in_table]
+        if missing_times:
+            #add the missing times to the time dimension table
+            self.session.bulk_insert_mappings(DimTime, missing_times)
+            self.session.commit()
+            # requery the whole time swath
+            times_in_db = self.session.query(DimTime).where(
+                and_((DimTime.time >= start_time),
+                (DimTime.time <= end_time))      
+            ).all()
+        times_in_db = {rec.time: rec.id for rec in times_in_db}
+        self.dimension_cache['time'] = self.dimension_cache['time'] | times_in_db
+        return self.dimension_cache['time'].get(time)
+            
+    def batch_insert_measurements(self, measurements):
+        try:
+            # filter out any alarms from the measurements
+            filtered_measurements = [
+                {key: value for key, value in measurement.items() if key != 'alarms'}
+                for measurement in measurements
+            ]
+            
+            # Build the core insert statement
+            stmt = insert(FactMeasurement).returning(FactMeasurement.id)
+            
+            # Execute with multiple rows of data
+            result = self.session.execute(stmt, filtered_measurements)
+            
+            # Fetch all returned IDs
+            inserted_ids = result.scalars().all()
+            
+            # Update measurements with the returned IDs
+            updated_measurements = [
+                {**measurement, "id": result_id}
+                for measurement, result_id in zip(measurements, inserted_ids)
+            ]
+            
+            return updated_measurements
+
+        except IntegrityError as e:
+            self.logger.error(f"batch_insert_measurements integrity error: {str(e)}")
+            return []
+        except Exception as e:
+            self.logger.error(f"batch_insert_measurements unknown error: {str(e)}")
+            return []    
+
+    def batch_insert_alarms(self, alarms):
+        if len(alarms) == 0:
+            return
+        try:            
+            # Build the core insert statement
+            stmt = insert(FactAlarm)
+            
+            # Execute with multiple rows of data
+            result = self.session.execute(stmt, alarms)
+       
+        except IntegrityError as e:
+            self.logger.error(f"batch_insert_alarms integrity error: {str(e)}")
+            return []
+        except Exception as e:
+            self.logger.error(f"batch_insert_alarms unknown error: {str(e)}")
+            return []    
+
+    def batch_insert_geolocations(self, geolocations):
+        if len(geolocations) == 0:
+            return
+        try:            
+            # Build the core insert statement
+            stmt = insert(DimGeolocation)
+            
+            # Execute with multiple rows of data
+            result = self.session.execute(stmt, geolocations)
+       
+        except IntegrityError as e:
+            self.logger.error(f"batch_insert_geolocations integrity error: {str(e)}")
+            return []
+        except Exception as e:
+            self.logger.error(f"batch_insert_geolocations unknown error: {str(e)}")
+            return []    
+
+    def merge_gps_coordinates(self,gps_records):
+        # Group records by common keys (platform_id, instrument_id, sample_time_id)
+        grouped = defaultdict(dict)
+        for record in gps_records:
+            key = (record['platform_id'], record['instrument_id'], record['sample_time_id'])
+            grouped[key].update(record)
+
+        # Build the final list of merged dictionaries
+        merged_records = [
+            {'platform_id': key[0], 'instrument_id': key[1], 'sample_time_id': key[2], 
+            'longitude': value.get('longitude'), 'latitude': value.get('latitude')}
+            for key, value in grouped.items()
+        ]
+        return merged_records
+        
+    def insert_subbatch(self, messages):
+        measurements = []
+        alarms = []
+        coordinates = []
+        # Prepare and batch insert measurements
+        for message in messages:
+            measurement = {
+                'platform_id': self.get_or_create_dimension(DimPlatform, 'platform', message['platform'], 'platform'),
+                'instrument_id': self.get_or_create_dimension(DimInstrument, 'instrument', message['instrument'], 'instrument'),
+                'parameter_id': self.get_or_create_dimension(DimParameter, 'parameter', message['parameter'], 'parameter'),
+                'unit_id': self.get_or_create_dimension(DimUnit, 'unit', message['unit'], 'unit'),
+                'acquisition_type_id': self.get_or_create_dimension(DimAcquisitionType, 'acquisition_type', message['acquisition_type'], 'acquisition_type'),
+                'sample_time': message.get('sample_time'),
+                'acquisition_time_id': self.get_or_create_time_dimension(message.get('acquisition_time')),
+                'instrument_time_id': self.get_or_create_time_dimension(message.get('instrument_time')),
+                'sample_time_id': self.get_or_create_time_dimension(message.get('sample_time')),
+                'value': message.get('value'),
+                'string': message.get('string'),
+                'alarms': message.get('alarms',[])
+            }
+            measurements.append(measurement)
+            # if the measurement is a GPS cordinate, prepare the coordinate for the geolocation dimension table
+            if message.get('acquisition_type') == 'GPS' and message['value']:
+                coordinate = {
+                    'platform_id': measurement['platform_id'],
+                    'instrument_id': measurement['instrument_id'],
+                    'sample_time_id': self.get_or_create_time_dimension(message.get('sample_time')),
+                    message['parameter']: message['value']
+                }
+                coordinates.append(coordinate)
+        measurements = self.batch_insert_measurements(measurements)
+        # Now that IDs have been assigned to the measurements, extracte and prep the alarms
+        for measurement in measurements:            
+            for alarm in measurement.get('alarms',[]):
+                alarm_rec = {
+                    'platform_id': measurement['platform_id'],
+                    'measurement_id': measurement['id'],
+                    'instrument_id': measurement['instrument_id'],
+                    'parameter_id': measurement['parameter_id'],
+                    'sample_time_id': self.get_or_create_time_dimension(measurement['sample_time']),
+                    'alarm_type_id': self.get_or_create_dimension(DimAlarmType, 'alarm_type', alarm['alarm_type'], 'alarm_type'),
+                    'alarm_level_id': self.get_or_create_dimension(DimAlarmLevel, 'alarm_level', alarm['alarm_level'], 'alarm_level'),
+                    'data_impacted': alarm['data_impacted'],           
+                    'alarm_message': alarm['alarm_message'],           
+                }
+                alarms.append(alarm_rec)
+        self.batch_insert_alarms(alarms)
+        if coordinates:
+            geolocations = self.merge_gps_coordinates(coordinates)
+            self.batch_insert_geolocations(geolocations) 
+        self.session.commit()
+        
+    def insert_batch(self, batch):
+        earliest_time = min(batch,key=lambda x:x['sample_time'])['sample_time'].replace(microsecond=0)
+        latest_time = max(batch,key=lambda x:x['sample_time'])['sample_time'].replace(microsecond=0)
+        times = []
+        mid_time = earliest_time
+        while mid_time <= latest_time:
+            times.append(mid_time)
+            mid_time += timedelta(seconds=self.insert_batch_seconds)
+        for time in times:
+            sub_batch = [rec for rec in batch if rec['sample_time'].replace(microsecond=0) == time]
+            batch = [rec for rec in batch if rec['sample_time'].replace(microsecond=0) != time]
+            self.insert_subbatch(sub_batch)   
+                    
+        
+
+        
 def submit_measurement(measurement, submit_time, config):
     # collect measurements, and store into files at configured intervals  
     global submissions
@@ -430,23 +674,38 @@ session = Session()
 submissions = []
 sumbission_start_time = datetime.now()
 
+inserter = Inserter(session, config)
+times = inserter.get_or_create_time_dimension(datetime.now()-timedelta(days=5))
+
+batch_insert = True 
+
+queued_recs_to_batch = config.get('queued_recs_to_batch', 1000)
 
 while True:
     message = []
     if collector_input == 'queue':
-        try:
-            message = queue.get()
-            #continue
-        except Exception as e:
-            logger.error("exception in get from queue")
-            logger.error(e)
-        else:
-            logger.debug(str(message))
+        while len(message) < queued_recs_to_batch:
+            try:
+                record = queue.get()
+                # GPS acquireres package their coordinates as lists
+                # to keep coords from being separated in batching
+                if isinstance(record,list):
+                    for r in record:
+                        message.append(r)
+                else:
+                    message.append(record)
+                #continue
+            except Exception as e:
+                logger.error("exception in get from queue")
+                logger.error(e)
+            else:
+                logger.debug(str(message))
     elif collector_input == 'submissions':
         files = get_submission_files(submission_file_directory, submit_file_pattren) 
         time.sleep(0.5)
         if files:
             file = files[0]
+            logger.info(f'Starting on submission file {file}')
             try:
                 message = get_messages_from_file(file['filename'])
             except Exception as e:
@@ -458,23 +717,33 @@ while True:
                 logger.info('Moved submission file {} to {}'.format(file['filename'], submitted_file_directory))
                         
     file_start_time = datetime.now()
-    
-    for measurement in message:
-        if 'acquisition_type' in measurement.keys():
-            #start_time = datetime.now()
-            success = insert_measurment_into_database(session, measurement)
-            #end_time = datetime.now()
-            #exec_secs = (end_time - start_time).total_seconds()
-            #print(f'message insert took {exec_secs} seconds')
-            if success:
-                #logger.debug("Measurement inserted successfully.")
-                pass
-            else:
-                logger.error("Measurement insertion failed: "+str(measurement))
-            #print(str(len(submissions))+'submissions to '+sumbission_start_time.strftime("%Y-%m-%d %H:%M:%S"))
-            if collector_input == 'queue':
-                if submit_measurement(measurement, sumbission_start_time, config):
-                    sumbission_start_time = datetime.now()
+
+    if message and batch_insert:
+        numRecords = len(message)
+        start_time = datetime.now()
+        inserter.insert_batch(message)
+        end_time = datetime.now()
+        exec_secs = (end_time - start_time).total_seconds()
+        logger.info(f"{numRecords} inserted, insert took {exec_secs} seconds, {exec_secs/numRecords} secs per record")
+    else:
+
+        for measurement in message:
+            if 'acquisition_type' in measurement.keys():
+                start_time = datetime.now()
+                success = insert_measurment_into_database(session, measurement)
+                end_time = datetime.now()
+                exec_secs = (end_time - start_time).total_seconds()
+                logger.info(f"record {measurement['parameter']} {measurement['acquisition_type']} insert took {exec_secs} seconds")
+                #print(f'message insert took {exec_secs} seconds')
+                if success:
+                    #logger.debug("Measurement inserted successfully.")
+                    pass
+                else:
+                    logger.error("Measurement insertion failed: "+str(measurement))
+                #print(str(len(submissions))+'submissions to '+sumbission_start_time.strftime("%Y-%m-%d %H:%M:%S"))
+                if collector_input == 'queue':
+                    if submit_measurement(measurement, sumbission_start_time, config):
+                        sumbission_start_time = datetime.now()
     if message:
         file_end_seconds = (datetime.now()-file_start_time).total_seconds()
         logger.info(f'message cluster (submit file) length {len(message)} messages processed in {file_end_seconds} seconds')
