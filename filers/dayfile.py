8f8d98aafd3a80bf27a34b564d0ea546e4106d56
robinweber <rjweber@berkeley.edu>
2025-02-18 20:26:22 +0000

    Uodated day filer to optionally write all data for a day (without geolocations)

--- filers/dayfile.py
+++ filers/dayfile.py
@@ -3,9 +3,11 @@ import os
 from datetime import datetime, timedelta
 import pandas as pd
 from sqlalchemy import create_engine, text
-from vandaq_2step_measurements_query import get_2step_query
+from vandaq_2step_measurements_query import get_2step_query_with_alarms
+from vandaq_2step_measurements_query import get_measurements_with_alarms_and_locations
 import pytz   # Or use from zoneinfo import ZoneInfo for Python 3.9+
 import yaml
+import argparse
 
 
 def add_timezone_column(df, time_col, target_tz):
@@ -43,6 +45,7 @@ def add_timezone_column(df, time_col, target_tz):
 
     return df
 
+parser = argparse.ArgumentParser(description='VanDAQ create daily data file starting at midnight')
 
 configfile_name = '/home/vandaq/vandaq/filers/dayfile.yaml'
 try:
@@ -58,14 +61,24 @@ if 'directory' in config:
     filedir = config['directory']
     
 # Replace with your PostgreSQL connection details
-DATABASE_URI = 'postgresql+psycopg2://vandaq:p3st3r@localhost:5432/vandaq-dev'
+DATABASE_URI = config['db_connect_string']
 
 # Establish database connection
 engine = create_engine(DATABASE_URI)
 
+gmt_timezone = pytz.timezone('GMT')
+
+
+
+parser.add_argument('data_date', nargs='?', default=None,
+                    help='the date of the data (local timezone)')
+parser.add_argument('--nogps', action='store_true',
+                    help='dump all data regardless of if there are geolocations')
+args = parser.parse_args()
+
 # Set date filter
-if len(sys.argv) > 1:
-    date_str = sys.argv[1]
+if args.data_date:
+    date_str = args.data_date
     try:
         query_date = datetime.strptime(date_str, '%Y-%m-%d')
     except ValueError:
@@ -74,26 +87,48 @@ if len(sys.argv) > 1:
 else:
     query_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
+no_gps = args.nogps
+
 # Define start and end of the date range (start_time is midnight, end_time is midnight of the next day)
 start_time = query_date.replace(hour=0, minute=0, second=0, microsecond=0)
 end_time = start_time + timedelta(days=1)
 
-gmt_timezone = pytz.timezone('GMT')
-
 for platform in config['platforms']:
     timezone = config[platform]['timezone']
+    main_gps = config[platform]['gps_instrument']
     local_tz = pytz.timezone(config[platform]['timezone'])
     gmt_start_time = (local_tz.localize(start_time)).astimezone(gmt_timezone)
     gmt_end_time = (local_tz.localize(end_time)).astimezone(gmt_timezone)
     destDir = os.path.join(filedir,config[platform]['subdir']) 
 
-    df = get_2step_query(engine, gmt_start_time, gmt_end_time, platform = platform)
+    if no_gps:
+        df = get_2step_query_with_alarms(engine, start_time=gmt_start_time, end_time=gmt_end_time, platform = platform, wide=False)
+    else:
+        df = get_measurements_with_alarms_and_locations(engine, start_time=gmt_start_time, end_time=gmt_end_time, gps_instrument=main_gps, platform = platform)
+    if len(df) > 0:
+        df = add_timezone_column(df, 'sample_time', 'US/Pacific')
 
-    df = add_timezone_column(df, 'sample_time', 'US/Pacific')
+        if len(df) == 0:
+            if no_gps:
+                print('no data for {parser.data_date}')
+            else:
+                print('no geolocation data for {parser.data_date}')
+            exit()
 
-    # Write DataFrame to CSV with header and formatted datetime columns
-    file_name = f"measurements_{platform}_{start_time.strftime('%Y-%m-%d')}.csv"
-    df.to_csv(os.path.join(filedir,file_name), index=False)
+        # Write DataFrame to CSV with header and formatted datetime columns
+        nogeo = ''
+        if no_gps:
+            nogeo = 'no-geolocations_'
+        file_name = f"measurements_{platform}_{start_time.strftime('%Y-%m-%d')}_{nogeo}long.csv"
+        df.to_csv(os.path.join(filedir,file_name), index=False)
 
-    print(f"Measurements saved to {file_name}")
+
+        # df = get_measurements_with_alarms_and_locations(engine, gmt_start_time, end_time=gmt_end_time, platform = platform, wide=True)
+        # df = add_timezone_column(df, 'sample_time', 'US/Pacific')
+
+        # # Write DataFrame to CSV with header and formatted datetime columns
+        # file_name = f"measurements_{platform}_{start_time.strftime('%Y-%m-%d')}_wide.csv"
+        # df.to_csv(os.path.join(filedir,file_name), index=False)
+
+        print(f"Measurements saved to {file_name}")
 
